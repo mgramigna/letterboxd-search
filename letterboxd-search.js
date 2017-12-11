c6e3b737d8b9db5ae3a5c70f2529a6635264f44b
Matt Gramigna <mgramigna@umass.edu>
2017-12-10 19:37:12 -0500

    Switched to cheerio and request promises

--- letterboxd-search.js
+++ letterboxd-search.js
@@ -1,35 +1,77 @@
-const scraperjs = require('scraperjs')
-module.exports.search = (title, cb) => {
+const request = require('request-promise');
+const cheerio = require('cheerio');
+const numeral = require('numeral');
+
+module.exports = { search };
+
+function search(title) {
+  return new Promise(function(resolve, reject) {
     const endpoint = title.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/ /g,'-')
-    scraperjs.StaticScraper.create(`https://letterboxd.com/film/${endpoint}`).scrape($ => {
-        const rt = $('.text-link').text().trim()
-        return {
-            title: title,
-            url: `https://letterboxd.com/film/${endpoint}`,
-            year: parseInt($('#featured-film-header p small a').text()),
-            director: $('#featured-film-header p a span').text(),
-            runtimeMinutes: parseInt(rt.substring(0,rt.indexOf('mins')-1)),
-            tagline: $('.tagline').text(),
-            synopsis: $('.truncate p').text(),
-            cast: $('.cast-list p a span').toArray().map(a => {
-                return a.children[0].data
-            }),
-            stats: {
-                watched: $('.filmstat-watches a').text(),
-                lists: $('.filmstat-lists a').text(),
-                likes: $('.filmstat-likes a').text()
-            },
-            averageRating: parseFloat($('.display-rating').text()),
-            starRatings: $('.rating-histogram-bar a').toArray().map(a => {
-                return a.children[0].data
-            }),
-            backdropImage: $('#backdrop').attr('data-backdrop'),
-            posterImage: $('.film-poster img').attr('src'),
-            relatedFilms: $('.poster-list li div img').toArray().map(a => {
-                return a.attribs.alt
-            })
-        }
-    }).then(res => {
-        cb(res);
-    })
+    request({
+      uri: `https://letterboxd.com/film/${endpoint}`,
+      transform(body) {
+        return cheerio.load(body);
+      }
+    }).then($ => {
+      const ratingsMap = {
+        'half-★': 0.5,
+        '★': 1.0,
+        '★½': 1.5,
+        '★★': 2.0,
+        '★★½': 2.5,
+        '★★★': 3.0,
+        '★★★½': 3.5,
+        '★★★★': 4.0,
+        '★★★★½': 4.5,
+        '★★★★★': 5.0
+      };
+      let obj = {
+        title: $('meta[name="twitter:title"]').attr('content'),
+        url: $('meta[property="og:url"]').attr('content'),
+        year: numeral($('.film-poster').attr('data-film-release-year')).value(),
+        director: $('meta[name="twitter:data1"]').attr('content'),
+        runtimeMinutes: numeral($('.text-link').text().trim()).value(),
+        tagline: $('.tagline').text(),
+        synopsis: $('meta[property="og:description"]').attr('content'),
+        cast: $('.cast-list').find('a').toArray().map(a => {
+          return {
+            actor: $(a).find('span').text(),
+            role: a.attribs.title,
+            url: `https://letterboxd.com${a.attribs.href}`
+          }
+        }),
+        stats: {
+            watched: numeral($('.filmstat-watches a').text()).value(),
+            lists: numeral($('.filmstat-lists a').text()).value(),
+            likes: numeral($('.filmstat-likes a').text()).value()
+        },
+        averageRating: numeral($('.display-rating').text()).value(),
+        ratings: $('.rating-histogram-bar a').toArray().map(a => {
+          let rating = a.children[0].data
+          let re = /[0-9]\s+/g
+          let index = re.exec(rating).index;
+          let data = rating.split(' ')
+          return {
+            stars: ratingsMap[data[0].substring(index+2)],
+            count: numeral(data[0].substring(0,index+1)).value(),
+            percentage: numeral(data[2].substring(1,data[2].indexOf('%'))).value()
+          }
+        }),
+        backdropImage: $('meta[property="og:image"]').attr('content'),
+        posterImage: $('.film-poster img').attr('src'),
+        relatedFilms: $('.poster-list li div').toArray().map(a => {
+            return {
+              name: $(a).find('img').attr('alt'),
+              url: `https://letterboxd.com${a.attribs['data-film-slug']}`
+            }
+        })
+      }
+      resolve(obj);
+    }).catch(err => {
+      reject({
+        name: err.name,
+        statusCode: err.statusCode,
+      });
+    });
+  });
 }
